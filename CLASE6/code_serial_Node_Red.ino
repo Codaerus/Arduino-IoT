5a3090041b73842ffea22ec87500c993038f43b6
Codaerus <christianqc91@gmail.com>
2025-09-01 16:27:51 -0500

    Create code_serial_Node_Red.ino

---
+++ CLASE6/code_serial_Node_Red.ino
@@ -0,0 +1,25 @@
+const char msg[30];
+unsigned long t = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(4,OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+}
+void loop() {
+  //Recepción
+  if(Serial.available()>0){//hay datos para leer? (buffer)
+
+    char x = Serial.read();
+    if (x == 'a'){digitalWrite(4,HIGH);}
+    if (x == 'b'){digitalWrite(4,LOW);}
+  }
+  
+  //Enviar
+  if(millis()-t >= 5000){
+      uint16_t pot1 = analogRead(32);
+      uint16_t pot2 = analogRead(35);
+      snprintf(msg,"%d,%d",pot1,pot2);
+      Serial.println(msg);
+      t = millis();
+    }
+}
